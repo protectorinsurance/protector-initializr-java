a4512ff73922bf7d6fc5425dee9d22161ad65882
John Mikael Gundersen <john.mikael.gundersen@protectorforsikring.no>
2021-06-21 07:42:51 +0200

    Initial web service commit

---
+++ web/src/test/java/no/protector/initializr/web/ApplicationTest.java
@@ -0,0 +1,13 @@
+package no.protector.initializr.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
