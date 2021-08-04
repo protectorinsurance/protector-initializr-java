2d4fd6928321fad5a1cd33dc5fd1836acded3c17
John Mikael Gundersen <john.mikael.gundersen@protectorforsikring.no>
2021-08-04 11:35:39 +0200

    #COMMONS-2458 Removing uneccesary printout and more efficient system test command

--- web-test/src/test/groovy/no/protector/initializr/system/test/AbstractSystemSpec.groovy
+++ web-test/src/test/groovy/no/protector/initializr/system/test/AbstractSystemSpec.groovy
@@ -2,7 +2,6 @@ package no.protector.initializr.system.test
 
 import no.protector.initializr.system.test.config.ContainerConfig
 import no.protector.initializr.system.test.config.EndpointConfig
-import no.protector.initializr.system.test.extensions.SystemTestContainerLogging
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 import org.testcontainers.spock.Testcontainers
@@ -10,7 +9,6 @@ import spock.lang.Specification
 
 @Testcontainers
 @SpringBootTest
-@SystemTestContainerLogging
 @ContextConfiguration(classes = [ContainerConfig, EndpointConfig])
 abstract class AbstractSystemSpec extends Specification {
 }
