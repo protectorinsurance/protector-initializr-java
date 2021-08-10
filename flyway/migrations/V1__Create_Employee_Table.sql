3903c4c896a59cf706cf97280d09e75bdc6ff047
John Mikael Gundersen <john.mikael.gundersen@protectorforsikring.no>
2021-08-10 13:24:44 +0200

    #COMMONS-2461 Adding application mutation

--- flyway/migrations/V1__Create_Employee_Table.sql
+++ flyway/migrations/V1__Create_Employee_Table.sql
@@ -1,6 +1,8 @@
+-- INITIALIZR:DATABASE
 create table Employee
 (
     Id         int          not null primary key,
     First_Name varchar(128) not null,
     Last_Name  varchar(128) not null
 )
+-- INITIALIZR:DATABASE
