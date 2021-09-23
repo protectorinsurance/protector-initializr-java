1782555e58f69953c042e71dc9d51eb3ffc58fb2
John Mikael Gundersen <john.mikael.gundersen@protectorforsikring.no>
2021-09-23 09:18:46 +0200

    #SA-362 Moving non-auto config to its own yml namespace

--- domain-no-database/src/main/java/no/protector/initializr/domain/configuration/KafkaConfiguration.java
+++ domain-no-database/src/main/java/no/protector/initializr/domain/configuration/KafkaConfiguration.java
@@ -1,50 +1,14 @@
 //INITIALIZR:KAFKA-PRODUCER
 package no.protector.initializr.domain.configuration;
 
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
 public class KafkaConfiguration {
-
-    private static final int BATCH_SIZE = 16384;
-    private static final int BUFFER_MEMORY = 33_554_432;
-
-    private final KafkaProperties kafkaProperties;
-
-    public KafkaConfiguration(KafkaProperties kafkaProperties) {
-        this.kafkaProperties = kafkaProperties;
-    }
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        return new KafkaAdmin(kafkaProperties.buildAdminProperties());
-    }
-
-    private Map<String, Object> getDefaultProducerConfig() {
-        Map<String, Object> props = new HashMap<>(kafkaProperties.buildProducerProperties());
-        props.putAll(Map.of(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers(),
-                ProducerConfig.ACKS_CONFIG, "all",
-                ProducerConfig.RETRIES_CONFIG, 0,
-                ProducerConfig.BATCH_SIZE_CONFIG, BATCH_SIZE,
-                ProducerConfig.LINGER_MS_CONFIG, 1,
-                ProducerConfig.BUFFER_MEMORY_CONFIG, BUFFER_MEMORY,
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class,
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class
-        ));
-        return props;
-    }
 
     /**
      * TODO: Implement producer templates and factories
@@ -57,8 +21,9 @@ public class KafkaConfiguration {
 
     //INITIALIZR:INITIALIZR-DEMO
     @Bean
-    public ProducerFactory<Integer, String> employeeProducerFactory() {
-        return new DefaultKafkaProducerFactory<>(getDefaultProducerConfig());
+    public ProducerFactory<Integer, String> employeeProducerFactory(
+            KafkaConfigurationProperties kafkaConfigurationProperties) {
+        return new DefaultKafkaProducerFactory<>(kafkaConfigurationProperties.toProducerConfig());
     }
 
     @Bean
---
+++ domain/src/main/java/no/protector/initializr/domain/configuration/KafkaConfigurationProperties.java
@@ -0,0 +1,48 @@
+//INITIALIZR:KAFKA-PRODUCER
+package no.protector.initializr.domain.configuration;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(value = "kafka")
+public class KafkaConfigurationProperties {
+
+    private static final String SCHEMA_REGISTRY_URL = "schema.registry.url";
+
+    private String bootstrapServers;
+    private String clientId;
+    private String schemaRegistryUrl;
+    private String acksConfig;
+
+    public Map<String, Object> toProducerConfig() {
+        return Map.of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+                ProducerConfig.CLIENT_ID_CONFIG, clientId,
+                ProducerConfig.ACKS_CONFIG, acksConfig,
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class,
+                SCHEMA_REGISTRY_URL, schemaRegistryUrl);
+    }
+
+    public void setBootstrapServers(String bootstrapServers) {
+        this.bootstrapServers = bootstrapServers;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setSchemaRegistryUrl(String schemaRegistryUrl) {
+        this.schemaRegistryUrl = schemaRegistryUrl;
+    }
+
+    public void setAcksConfig(String acksConfig) {
+        this.acksConfig = acksConfig;
+    }
+}
+//INITIALIZR:KAFKA-PRODUCER
